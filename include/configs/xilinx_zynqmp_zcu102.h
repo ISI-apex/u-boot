69c20509c1bcb81131e55dd5daf3b6ac1551ebd5
Alexei Colin <acolin@isi.edu>
2018-09-11 09:22:14 -0400

    config zcu102: disable HW not in Chiplet or unused

--- include/configs/xilinx_zynqmp_zcu102.h
+++ include/configs/xilinx_zynqmp_zcu102.h
@@ -10,6 +10,7 @@
 #ifndef __CONFIG_ZYNQMP_ZCU102_H
 #define __CONFIG_ZYNQMP_ZCU102_H
 
+#if 0
 #define CONFIG_ZYNQ_SDHCI1
 #define CONFIG_ZYNQ_I2C0
 #define CONFIG_ZYNQ_I2C1
@@ -46,6 +47,7 @@
 #define CONFIG_ZYNQ_EEPROM_BUS		5
 #define CONFIG_ZYNQ_GEM_EEPROM_ADDR	0x54
 #define CONFIG_ZYNQ_GEM_I2C_MAC_OFFSET	0x20
+#endif
 
 #include <configs/xilinx_zynqmp.h>
 
