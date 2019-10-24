c43f530d5c17f0cd81f67fa4d0f9ebaf5b765a43
Alexei Colin <acolin@isi.edu>
2019-10-24 17:37:01 -0400

    armv8: cpu: opt workaround for dcache_disable hang
    Hangs in ZeBu emulator. Add this workaround, but keep disabled by default.

--- arch/arm/cpu/armv8/cache_v8.c
+++ arch/arm/cpu/armv8/cache_v8.c
@@ -473,6 +473,7 @@ void dcache_enable(void)
 
 void dcache_disable(void)
 {
+#ifndef WORKAROUND_NO_DISABLE_DCACHE
 	uint32_t sctlr;
 
 	sctlr = get_sctlr();
@@ -485,6 +486,7 @@ void dcache_disable(void)
 
 	flush_dcache_all();
 	__asm_invalidate_tlb_all();
+#endif /* WORKAROUND_NO_DISABLE_DCACHE */
 }
 
 int dcache_status(void)
