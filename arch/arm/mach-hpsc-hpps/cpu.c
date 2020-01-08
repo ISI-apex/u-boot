2c8dd8270d348c30290e62b22ba23f725d3bb760
Alexei Colin <acolin@isi.edu>
2020-01-08 11:10:09 -0800

    arm: hpsc-hpps: fill DRAM banks in memmap
    The function came that filled DRAM bank regions in MMU map came from
    Xilinx ZynqMP code, but was disabled in HPSC fork. Re-enable it.
    
    Remove the statically defined entires for DRAM.
    Also remove other entires, and leave only the peripheral entry,
    that is the only one needed by U-boot.
    
    Also, fix the logic for terminating the variable-length mem map array.
    After this patch, the code does not rely on mem map array being zeroed
    by .bss init.

--- arch/arm/mach-hpsc-hpps/cpu.c
+++ arch/arm/mach-hpsc-hpps/cpu.c
@@ -5,102 +5,65 @@
 
 DECLARE_GLOBAL_DATA_PTR;
 
-#define HPSC
+/* Helpers for printf'ing 64-bit values (%016llx breaks) */
+#define U64_FMT "0x%08lx%08lx"
+#define U64_ARG(x) (unsigned long)((x) >> 32), (unsigned long)(x)
 
-/*
- * Number of filled static entries and also the first empty
- * slot in hpps_mem_map.
- */
-#define HPPS_MEM_MAP_USED	6
+/* Number of filled static entries in hpps_mem_map. */
+#define HPPS_MEM_MAP_STATIC	1
 
 /* +1 is end of list which needs to be empty */
-#define HPPS_MEM_MAP_MAX (HPPS_MEM_MAP_USED + CONFIG_NR_DRAM_BANKS + 1)
+#define HPPS_MEM_MAP_MAX (HPPS_MEM_MAP_STATIC + CONFIG_NR_DRAM_BANKS + 1)
 
+/* If you add/remove entries, make sure to update HPPS_MEM_MAP_STATIC */
 static struct mm_region hpps_mem_map[HPPS_MEM_MAP_MAX] = {
-	{
-		.virt = 0x80000000UL,
-		.phys = 0x80000000UL,
-		.size = 0x40000000UL,
-		.attrs = PTE_BLOCK_MEMTYPE(MT_NORMAL) |
-			 PTE_BLOCK_INNER_SHARE
-	},
-	{
-		.virt = 0x100000000UL,
-		.phys = 0x100000000UL,
-		.size = 0x80000000UL,
-		.attrs = PTE_BLOCK_MEMTYPE(MT_NORMAL) |
-			 PTE_BLOCK_INNER_SHARE
-	},
-	{
-		.virt = 0x200000000UL,
-		.phys = 0x200000000UL,
-		.size = 0x200000000UL,
-		.attrs = PTE_BLOCK_MEMTYPE(MT_NORMAL) |
-			 PTE_BLOCK_INNER_SHARE
-	},
-	{
-		.virt = 0xC0000000UL,
-		.phys = 0xC0000000UL,
-		.size = 0x30000000UL,
+	{ /* HPPS peripherals */
+		.virt = 0xf9000000UL,
+		.phys = 0xf9000000UL,
+		.size = 0x07000000UL,
 		.attrs = PTE_BLOCK_MEMTYPE(MT_DEVICE_NGNRNE) |
 			 PTE_BLOCK_NON_SHARE |
 			 PTE_BLOCK_PXN | PTE_BLOCK_UXN
 	},
-	{
-		.virt = 0xf8000000UL,
-		.phys = 0xf8000000UL,
-		.size = 0x07e00000UL,
-		.attrs = PTE_BLOCK_MEMTYPE(MT_DEVICE_NGNRNE) |
-			 PTE_BLOCK_NON_SHARE |
-			 PTE_BLOCK_PXN | PTE_BLOCK_UXN
-	}, {
-		.virt = 0x400000000UL,
-		.phys = 0x400000000UL,
-		.size = 0x400000000UL,
-		.attrs = PTE_BLOCK_MEMTYPE(MT_DEVICE_NGNRNE) |
-			 PTE_BLOCK_NON_SHARE |
-			 PTE_BLOCK_PXN | PTE_BLOCK_UXN
-	},
-	{
-		.virt = 0x1000000000UL,
-		.phys = 0x1000000000UL,
-		.size = 0xf000000000UL,
-		.attrs = PTE_BLOCK_MEMTYPE(MT_DEVICE_NGNRNE) |
-			 PTE_BLOCK_NON_SHARE |
-			 PTE_BLOCK_PXN | PTE_BLOCK_UXN
-	},
-	{
-		.virt = 0,
-		.phys = 0,
-		.size = 0,
-		.attrs = 0
-	}
+	/* Variable number of entries for DRAM j start here, at index
+	 * HPPS_MEM_MAP_STATIC, when DRAM bank entries are populated in
+	 * mem_map_fill, the list is terminated by an entry with all zeroes. */
 };
+
+struct mm_region *mem_map = hpps_mem_map;
 
 /* Called from code in board/ */
 void mem_map_fill(void)
 {
-	int banks = HPPS_MEM_MAP_USED;
-
-#ifdef HPSC
-	return ;
-#endif
-
-	for (int i = 0; i < CONFIG_NR_DRAM_BANKS; i++) {
+	int i, j;
+	for (i = 0, j = HPPS_MEM_MAP_STATIC; i < CONFIG_NR_DRAM_BANKS; i++, j++) {
 		/* Zero size means no more DDR that's this is end */
 		if (!gd->bd->bi_dram[i].size)
 			break;
-
-		hpps_mem_map[banks].virt = gd->bd->bi_dram[i].start;
-		hpps_mem_map[banks].phys = gd->bd->bi_dram[i].start;
-		hpps_mem_map[banks].size = gd->bd->bi_dram[i].size;
-		hpps_mem_map[banks].attrs = PTE_BLOCK_MEMTYPE(MT_NORMAL) |
+		hpps_mem_map[j].virt = gd->bd->bi_dram[i].start;
+		hpps_mem_map[j].phys = gd->bd->bi_dram[i].start;
+		hpps_mem_map[j].size = gd->bd->bi_dram[i].size;
+		hpps_mem_map[j].attrs = PTE_BLOCK_MEMTYPE(MT_NORMAL) |
 			PTE_BLOCK_INNER_SHARE;
-		banks = banks + 1;
 	}
-}
+	/* Write terminator entry for the variable-length list */
+	assert(j < HPPS_MEM_MAP_MAX);
+	hpps_mem_map[j].virt = 0;
+	hpps_mem_map[j].phys = 0;
+	hpps_mem_map[j].size = 0;
+	hpps_mem_map[j].attrs = 0;
 
-struct mm_region *mem_map = hpps_mem_map;
+#ifdef DEBUG
+	debug("\nMMU map (virt -> phys, size):\n");
+	for (j = 0; j < HPPS_MEM_MAP_MAX && hpps_mem_map[j].size; j++) {
+		/* breaks for 64-bit %llx (incl. breaking later execution) */
+		debug(U64_FMT " -> " U64_FMT " sz " U64_FMT "\n",
+			U64_ARG(hpps_mem_map[j].virt),
+			U64_ARG(hpps_mem_map[j].phys),
+			U64_ARG(hpps_mem_map[j].size));
+	}
+#endif /* DEBUG */
+}
 
 
 #ifdef CONFIG_CLOCKS
